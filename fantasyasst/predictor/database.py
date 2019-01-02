5e54ab4ff5863abe971392e0bdc13c8547ad789b
Lyndon Shi <shilyndon@gmail.com>
2019-01-02 00:30:24 -0500

    test connection with db

--- fantasyasst/predictor/database.py
+++ fantasyasst/predictor/database.py
@@ -4,12 +4,15 @@ from fantasyasst import *
 
 
 class Statistics:
+    RK = 'Rk'
+    TM = 'Tm'
+
     def __init__(self, league):
         """
         Construct Statistics object, which provides an interface to a
         Google Datastore instance with player data
 
-        :param league: member of enum League defining the league for which
+        :param league: enum of League defining the league for which
             statistics should be fetched
         :type league: League
         """
@@ -22,5 +25,13 @@ class Statistics:
     def _league(self):
         return self.league.value
 
-    def get_team_statistics(self, team, n_games, weight):
-        pass
+    def get_team_statistics(self, team, season, game, n_games_back, weight):
+        query = self.client.query(kind=str(season))
+        print(self.client.key(self._league, team))
+        if game - n_games_back < 1:
+            raise ValueError('Cross season retrieval not yet implemented')
+        query.add_filter(Statistics.RK, '<', game)
+        query.add_filter(Statistics.RK, '>=', game - n_games_back)
+        query.add_filter(Statistics.TM, '=', team)
+
+        return list(query.fetch())
--- test/unit/predictor/database.py
+++ test/unit/predictor/database.py
@@ -5,13 +5,27 @@ from fantasyasst.predictor.database import Statistics
 
 
 class TestStatistics(unittest.TestCase):
+    def setUp(self):
+        self.fetchers = {}
+        for league in League:
+            if league != League.NBA:
+                continue
+            self.fetchers[league] = Statistics(league)
+
     def test_league_set_on_construction(self):
-        str_repr = {
+        d = {
             League.NBA: 'NBA',
             League.NFL: 'NFL',
             League.MLB: 'MLB'
         }
         for league in League:
-            stat_fetcher = Statistics(league)
-            self.assertEqual(league, stat_fetcher.league)
-            self.assertEqual(str_repr[league], stat_fetcher._league)
+            if league != League.NBA:
+                continue
+            self.assertEqual(league, self.fetchers[league].league)
+            self.assertEqual(d[league], self.fetchers[league]._league)
+
+    def test_get_team_statistics(self):
+        stats = self.fetchers[League.NBA].get_team_statistics(
+            'CHI', 2018, 82, 5, 1
+        )
+        print(stats)
