312c7c2a377f1e85f1a53048c658af479027d2a8
Lyndon Shi <shilyndon@gmail.com>
2019-01-13 16:17:58 -0500

    test player history retrieval

--- test/unit/predictor/test_database.py
+++ test/unit/predictor/test_database.py
@@ -26,6 +26,8 @@ class TestStatistics(unittest.TestCase):
         for league, fetcher in self.fetchers.items():
             self.assertRaises(ValueError, fetcher.get_team_statistics,
                               'CHI', 2018, 3, 5)
+            self.assertRaises(ValueError, fetcher.get_player_statistics,
+                              'Jimmy Butler', 'CHI', 2018, 3, 5)
 
     def test_get_nba_team_statistics(self):
         team = 'CHI'
@@ -50,6 +52,37 @@ class TestStatistics(unittest.TestCase):
             self.assertEqual(team, entity.key.parent.name)
             self.assertEqual(entity.key.id, entity['Rk'])
             self.assertEqual(team, entity['Tm'])
+            needed_games.remove(entity.key.id)
+
+        self.assertFalse(needed_games)
+
+    def test_get_nba_player_statistics(self):
+        name = 'James Harden'
+        team = 'HOU'
+        season = 2018
+        game = 82
+        n = 5
+        entities = self.fetchers[League.NBA].get_player_statistics(
+            name, team, season, game, n
+        )
+        necessary_stats = {'3P', '3P%', '3PA', 'AST', 'AST%', 'BLK', 'BLK%',
+                           'DRB', 'DRB%', 'DRtg', 'FG', 'FG%', 'FGA', 'FT',
+                           'FT%', 'FTA', 'G', 'MP', 'Name', 'ORB', 'ORB%',
+                           'ORtg', 'Opp', 'PF', 'PTS', 'Rk', 'STL', 'STL%',
+                           'TOV', 'TOV%', 'TRB', 'TRB%', 'TS%', 'Tm', 'USG%',
+                           'eFG%'}
+
+        needed_games = [game - i for i in range(1, n + 2)]
+        for entity in entities:
+            for stat in necessary_stats:
+                self.assertIn(stat, entity)
+            self.assertEqual('2018', entity.key.kind)
+            self.assertGreater(game, entity.key.id)
+            self.assertLessEqual(game - n - 1, entity.key.id)
+            self.assertEqual('NBA', entity.key.parent.kind)
+            self.assertEqual(entity.key.id, entity['Rk'])
+            self.assertEqual(team, entity['Tm'])
+            self.assertEqual(name, entity['Name'])
             needed_games.remove(entity.key.id)
 
         self.assertFalse(needed_games)
